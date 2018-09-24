0def3057bdfb3bd7e7ca3f3032b1cf681e686145
LauritsStokholm <laurits.stokholm@gmail.com>
2018-09-24 12:53:42 +0200

    Compton scattering

--- ex3/code.py
+++ ex3/code.py
@@ -1,1 +1,163 @@
+# # # # # # # # # # # # # # Loading Libraries # # # # # # # # # # # # # # # # 
+# Used for basic commands
+import os
+
+# Data Analysis and basic mathematics
+import pandas as pd
+import numpy as np
+import scipy as sp
+from scipy.stats import norm
+from scipy.optimize import curve_fit
+from scipy import optimize as opt
+from scipy import stats 
+from scipy import signal
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-deep')
+
+# Matplotlib compatible TeX
+params = {'legend.fontsize'     : '20',
+          'axes.labelsize'      : '20',
+          'axes.titlesize'      : '20',
+          'xtick.labelsize'     : '20',
+          'ytick.labelsize'     : '20',
+          'legend.numpoints'    : 1,
+          'text.latex.preamble' : [r'\usepackage{siunitx}',
+                                   r'\usepackage{amsmath}'],
+          #'axes.spines.right'   : False,
+          #'axes.spines.top'     : False,
+          'figure.figsize'      : [8.5, 6.375],
+          'legend.frameon'      : False
+          }
+
+plt.rcParams.update(params)
+plt.rc('text',usetex =True)
+plt.rc('font', **{'family' : "sans-serif"})
+
+# This is for awesome zoom
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+
 # Preperations opgaverne
+def prep():
+    # Find the expectation relation between the scattering angle and the energy of
+    # the scattered photon for Compton scattering of photons from 137Cs
+
+    # Constants
+    # # Electron mass
+    m_e = 0.511 # MeV (mc2)
+    # # Electron radius (classical)
+    r0 = 2.818 # fm
+    # # Fine structure
+    alpha = 1/137
+    # # Energy
+    E_Cs137 = 662 * 10**(-3) # MeV
+
+
+    # Scattering angle vs energy
+    theta_rad = np.arange(0, np.pi, 0.01)
+    theta_deg = theta_rad * (180/np.pi)
+
+    # Compton scattering formula
+    # # E1 is gamma energy before scattering
+    # # m is the electron mass  (mc2)
+    # # theta is scattering angle
+
+    E_2 = lambda E_1, theta: E_1 / (1 + (E_1 / m_e) * (1-np.cos(theta)))
+
+    plt.figure()
+    plt.title("Preperation")
+    plt.xlabel("Scattering angle")
+    plt.ylabel(r"Scattering energy \ [$\si{\mega\electronvolt}$]")
+    plt.grid()
+    plt.plot(theta_deg, E_2(E_Cs137, theta_rad), label="Theoretical energy")
+    plt.legend()
+
+
+    # Calculate the variation of the cross section as a function of the
+    # scattering angle for Compton scattering of photons from Cs137.
+
+    dsdo = lambda theta: \
+             (r0**2) * (1 / (1 + alpha*(1 - np.cos(theta) ) ) )**3 \
+             * ( (1 + np.cos(theta) ) / 2) \
+             * ( 1 + alpha**2 * ( 1 - np.cos(theta) )**2 / ( (1+
+                 (np.cos(theta))**2) * (1 + alpha*(1 - np.cos(theta)))))
+
+    plt.figure()
+    plt.plot(theta_deg, dsdo(theta_rad), label="Theoretical compton cross section")
+    plt.grid()
+    plt.xlabel("Scattering angle")
+    plt.ylabel("Cross section")
+    plt.legend()
+    return
+
+# Experimental
+# # # # # # # # # # # # # # Importing Data # # # # # # # # # # # # # # # # # #
+""" Here we import the relevant data, and insert paths, directories in lists"""
+
+# Current working directory
+data_path     = os.path.join(os.getcwd(), 'data/')
+data_path_cal = os.path.join(os.getcwd(), 'data/calibration/')
+
+# List of files in ./Data
+data_files     = os.listdir(data_path)
+data_files_cal = os.listdir(data_path_cal)
+
+# Filtering files
+data     = [x for x in data_files if 'csv' in x]
+data_cal = [x for x in data_files_cal if 'csv' in x]
+
+# Paths for chosen datafiles
+data_dir     = [os.path.join(data_path, x) for x in data]
+data_dir_cal = [os.path.join(data_path_cal, x) for x in data_cal]
+
+
+# # # # # # # # # # # # # # Fitting Functions  # # # # # # # # # # # # # # # # 
+""" Here we define the functions for which we will fit the parameters of"""
+
+# Single Gaussian (3 parameters)
+def gaussian(x, A, mu, std):
+    return A * np.exp(-(x - mu)**2 / (2 * std**2))
+
+# Linear function (2 parameters)
+def linear(x, a, b):
+    return a*x + b
+
+# # # # # # # # # # # # # # DataFrame Import # # # # # # # # # # # # # # # # # 
+""" Here we import given list of directories into dataframes """
+
+def Dataframe(directory):
+
+    # Preparing dataframe structure
+    df = pd.DataFrame()
+
+    # Itterating over all files
+    file_names = []
+    for i in range(len(directory)):
+        # File name
+        file_names.append(os.path.basename(directory[i]))
+
+        # Reading csv
+        column_names = [file_names[i]]
+        df1 = pd.read_csv(directory[i], delimiter=',', index_col=0, names=column_names)
+        df1 = df1.loc[df1[file_names[i]] > 100]
+
+        # Concatenating
+        df = pd.concat([df, df1[column_names[0]]], axis=1)
+
+    # We make Dataframes and return file name
+    return df, file_names
+
+
+
+
+
+
+# Output
+#prep()
+
+df_cal, file_names_cal = Dataframe(data_dir_cal)
+
+df_cal.plot()
+
+
+plt.show()
